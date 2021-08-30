e565016eaa68531d8c726ffc30d688447a950089
unknown <jaymaina98@gmail.com>
2021-08-30 20:53:10 +0300

    Updates

---
+++ precache-manifest.f1bb4c45f8757abf19918c23d70890db.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c20ce9dc58ddaf78b29e91107222aa1",
+    "url": "/eldohubchallenge1/index.html"
+  },
+  {
+    "revision": "c4a9ec9598102ad637eb",
+    "url": "/eldohubchallenge1/static/js/2.b11bd800.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/eldohubchallenge1/static/js/2.b11bd800.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "815769d176f9d47201fb",
+    "url": "/eldohubchallenge1/static/js/main.6b6a0813.chunk.js"
+  },
+  {
+    "revision": "b90ea18e1ffd5993ef55",
+    "url": "/eldohubchallenge1/static/js/runtime-main.34d8c603.js"
+  }
+]);
